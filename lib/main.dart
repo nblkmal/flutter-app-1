8b17c0faf04d93104d9e77492657e2d6115666cd
nblkmal <nblkmal@gmail.com>
2021-09-08 17:18:35 +0800

    update stateless Home

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,38 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Text('IRING My'),
+    home: Home(),
   ));
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('IRING'),
+        centerTitle: true,
+        backgroundColor: Colors.purpleAccent,
+      ),
+      body: Center(
+        child: Text(
+          'Hello Chaperone!',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontFamily: 'Urbanist',
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          //  code function
+        },
+        child: Text('Book!'),
+        backgroundColor: Colors.purpleAccent,
+      ),
+    );
+  }
 }
